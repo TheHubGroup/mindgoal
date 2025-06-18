1b95a9b6fa1709bc5bafbafe71c605b0b51836a7
TheHubGroup <he@thehubgroup.net>
2025-06-17 20:35:47 -0500

    Updated WelcomeModal.tsx

--- src/components/WelcomeModal.tsx
+++ src/components/WelcomeModal.tsx
@@ -113,7 +113,7 @@ const WelcomeModal: React.FC<WelcomeModalProps> = ({ isOpen, onClose }) => {
               Bienvenido a <span className="font-bold text-yellow-300">Mind Goal</span>. 
               <br />
               <span className="text-lg md:text-xl lg:text-2xl text-yellow-200 font-semibold animate-pulse">
-                Haz clic en cualquier lugar para continuar
+                Haz clic en la X de la esquina para continuar
               </span>
             </div>
           </div>
