8b9d69ecf42d60d58c84b721f116cd94f82467cd
Hugo <he@thehubgroup.net>
2025-08-11 16:52:41 -0500

    Updated SemaforoLimites.tsx

--- src/pages/SemaforoLimites.tsx
+++ src/pages/SemaforoLimites.tsx
@@ -30,7 +30,8 @@ const situations: Situation[] = [
   {
     id: 'situacion_1',
     title: 'Alguien te quita algo sin tu permiso',
-    imageUrl: 'https://viact.org/imagenes/limite_01.png?auto=compress&cs=tinysrgb&w=400',
+    imageUrl: 'https://viact.org/imagenes/limite_01.png?
+    auto=compress&cs=tinysrgb&w=400',
     description: 'Alguien de tu salón toma algo que es tuyo, no te avisó que lo necesitaba. ¿Qué te parece ese comportamiento? Exprésalo en el Semáforo.'
   },
   {
