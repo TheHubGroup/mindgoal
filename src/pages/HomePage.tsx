383aa9434067b7397c63cb91161ca0027d005387
Hugo <he@thehubgroup.net>
2025-08-11 09:20:42 -0500

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -315,7 +315,7 @@ const HomePage = () => {
             </div>
 
             {/* Mensaje principal */}
-            <div className="space-y-8 max-w-4xl">
+            <div className="space-y-4 max-w-4xl">
               <h1 
                 className="text-4xl md:text-6xl lg:text-8xl font-bold text-white leading-tight"
                 style={{ 
