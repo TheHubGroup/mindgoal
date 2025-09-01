a0232489825d01bd281a0d9be0bcafb49625f421
Hugo <he@thehubgroup.net>
2025-09-01 12:39:33 -0500

    Fix Scene2A missing image with Decision_A_01.png

--- src/pages/DulcesMagicos.tsx
+++ src/pages/DulcesMagicos.tsx
@@ -195,6 +195,7 @@ const DulcesMagicos = () => {
                   width: '400px',
                   height: '400px',
                   maxWidth: '100%',
+                  height: 'auto',
                   objectFit: 'cover'
                 }}
                 onError={(e) => {
